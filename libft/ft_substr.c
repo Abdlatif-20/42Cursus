d44730720a8670b5f7a32312b2554b9a040ab255
ABDELLATYF EN-NEIYMY <71047666+Abdlatif-20@users.noreply.github.com>
2022-10-25 15:44:34 +0100

    Add files via upload

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aben-nei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 21:02:25 by aben-nei          #+#    #+#             */
-/*   Updated: 2022/10/24 17:01:53 by aben-nei         ###   ########.fr       */
+/*   Updated: 2022/10/25 03:11:38 by aben-nei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,28 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
-	char	*src;
 
-	src = (char *)s;
-	str = malloc(len + 1);
-	if (len > ft_strlen(s))
+	if (!s)
 		return (NULL);
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	if (start > ft_strlen(s))
+	{
+		str = malloc(sizeof(char));
+		if (!str)
+			return (NULL);
+		str[0] = 0;
+		return (str);
+	}
+	str = malloc(len + 1);
 	if (!str || !s)
 		return (NULL);
 	i = 0;
-	while (i < len && start < ft_strlen(src))
+	while (i < len && start <= ft_strlen(s))
 	{
-		str[i++] = *(src + start);
+		str[i++] = *(s + start);
 		start++;
 	}
 	str[i] = '\0';
 	return (str);
 }
-/*
-#include<stdio.h>
-int main()
-{
-	char * str = strdup("1");
-	char *s = ft_substr(str, 42, 42000000);
-	//char ptr[] = "Student At 1337 School";
-	//char *str = ft_substr("tripouille", 0, 42000);
-	printf("%s\n", s);
-}*/
