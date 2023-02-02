3dfc6b489651842b439e1572de95db81e485ebf7
Abdellatyf En-Neiymy <aben-nei@e2r10p11.1337.ma>
2023-02-02 03:13:08 +0100

    minitalk1

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: aben-nei <aben-nei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 04:51:52 by aben-nei          #+#    #+#             */
-/*   Updated: 2023/01/31 21:53:51 by aben-nei         ###   ########.fr       */
+/*   Updated: 2023/02/02 01:12:27 by aben-nei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@
 # include<string.h>
 
 int		ft_atoi(const char *str);
-void	ft_putnbr_fd(pid_t n, int fd);
+void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
-
+void	handler(int sig, siginfo_t *info, void *cont);
 #endif
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: aben-nei <aben-nei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 21:43:06 by aben-nei          #+#    #+#             */
-/*   Updated: 2023/02/01 22:30:37 by aben-nei         ###   ########.fr       */
+/*   Updated: 2023/02/02 01:11:51 by aben-nei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,20 @@ char	convert_to_char(char *str)
 }
 
 //handler function to handle signals from client
-void	handler(int sig)
+void	handler(int sig, siginfo_t *info, void *cont)
 {
+	(void)cont;
 	static char	str[9];
 	static int	i;
+	int			crent;
+	static int	get_pid;
 
+	crent = info->si_pid;
+	if (crent != get_pid)
+	{
+		get_pid = crent;
+		i = 0;
+	}
 	if (sig == SIGUSR1)
 		str[i] = '1';
 	else if (sig == SIGUSR2)
@@ -43,28 +52,23 @@ void	handler(int sig)
 	if (i == 8)
 	{
 		str[i] = '\0';
-		if (convert_to_char(str) == '\0')
-		{
-			ft_putchar_fd('\n', 1);
-			i = 0;
-		}
-		else
-		{
-			ft_putchar_fd(convert_to_char(str), 1);
-			i = 0;
-		}
+		ft_putchar_fd(convert_to_char(str), 1);
+		i = 0;
 	}
 }
 
 int	main(void)
 {
-	pid_t	pid;
+	pid_t				pid;
+	struct sigaction	sa;
 
 	pid = getpid();
 	ft_putnbr_fd(pid, 1);
 	ft_putchar_fd('\n', 1);
-	signal(SIGUSR1, handler);
-	signal(SIGUSR2, handler);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 		pause();
 }
